b0b5d4b0ce7b0e7a0394c73aef265532e07611a4
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2024-10-05 14:14:37 -0300

    Minor adjusts.

--- ptock.py
+++ ptock.py
@@ -33,12 +33,17 @@ class ScreenConnector:
 
     def __init__(self, color: PixelColor = PixelColor.GREEN) -> None:
         self.stdscr: curses.window = None
-        self.color: int = color.value  # Store the integer value of the color enum
+        self.color = color.value  # Store the integer value of the color enum
+
+    def draw_pixel(self, x: int = 0, y: int = 0) -> None:
+        """Draw a pixel at the specified (x, y) coordinates."""
+        color_pair = curses.color_pair(self.color)
+        self.stdscr.addch(y, x, "█", color_pair)
 
     def colors_init(self) -> None:
         """Initialize the color system for use in the terminal."""
-        curses.initscr()
-        curses.start_color()
+        curses.initscr()  # Initialize the curses mode
+        curses.start_color()  # Enable color functionality
 
         # Initialize color pairs
         for color in PixelColor:
@@ -46,13 +51,8 @@ class ScreenConnector:
                 color.value, getattr(curses, f"COLOR_{color.name}"), curses.COLOR_BLACK
             )
 
-    def draw_pixel(self, x: int = 0, y: int = 0) -> None:
-        """Draw a pixel at the specified (x, y) coordinates."""
-        color_pair = curses.color_pair(self.color)
-        self.stdscr.addch(y, x, "█", color_pair)
-
     def mount_screen(self) -> None:
-        """Mount the screen and draw initial graphics."""
+        """Draw the initial pixel and refresh the screen."""
         self.draw_pixel(0, 0)  # Draw the pixel at (0, 0)
         self.stdscr.refresh()  # Update the screen after all drawing operations
 
@@ -62,12 +62,14 @@ class ScreenConnector:
         self.colors_init()  # Initialize colors for curses
         curses.curs_set(0)  # Hide the cursor for better visual presentation
         self.mount_screen()  # Mount the screen and draw the pixel
-        self.stdscr.getch()  # Wait for user input to exit
+
+        # Wait for user input to exit
+        self.stdscr.getch()
         self.stdscr.clear()  # Clear the screen to remove artifacts
 
     def run(self) -> None:
         """Run the curses application."""
-        curses.wrapper(self.application)
+        curses.wrapper(self.application)  # Start the application with curses wrapper
 
 
 def ptock() -> None:
